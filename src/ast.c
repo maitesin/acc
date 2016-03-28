cdcfa4d99c7a01678c23b96430c2b3396867236a
Maitesin <violador.de.segmentos@gmail.com>
2016-03-28 19:12:47 +0100

    Implemented the stuff regarding to the previous commit.

--- src/ast.c
+++ src/ast.c
@@ -34,6 +34,15 @@ void init_node_if(node_if * node, ast_base * expression)
 	node->expression = expression;
 }
 
+void init_node_boolean_operator(node_boolean_operator * node, enum boolean_operator_type
+			   type, ast_base * first, ast_base * second)
+{
+	node->base.type = A_BOOLEAN_OPERATOR;
+	node->oper = type;
+	node->first = first;
+	node->second = second;
+}
+
 /*
  * Release functions implementation
  */
@@ -54,3 +63,41 @@ void free_node_return(node_return * node)
 {
 	free(node);
 }
+
+enum boolean_operator_type set_node_boolean_op_value(token_boolean_op * token)
+{
+	size_t len = strnlen(token->op,2);
+	if (len == 2)
+	{
+		if (strncmp(token->op, "==", 2))
+			return B_EQUALEQUAL;
+		else if (strncmp(token->op, "!=", 2))
+			return B_NOTEQUAL;
+		else if (strncmp(token->op, "<=", 2))
+			return B_LTEQUAL;
+		else if (strncmp(token->op, ">=", 2))
+			return B_GTEQUAL;
+		else
+		{
+			fprintf(stderr, "Error, %s cannot be proccessed as a boolean operator\n", token->op);
+			exit(-1);
+
+		}
+	}
+	else
+	{
+		switch(token->op[0])
+		{
+			case '!':
+				return B_NOT;
+			case '<':
+				return B_LT;
+			case '>':
+				return B_GT;
+			default:
+				fprintf(stderr, "Error, %s cannot be proccessed as a boolean operator\n", token->op);
+				exit(-1);
+		}
+	}
+}
+
