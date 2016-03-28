5a33526a3d69b4cec2e321f6b14bc28f3a806661
Maitesin <violador.de.segmentos@gmail.com>
2016-03-28 19:14:49 +0100

    Removed unnecesary stuff and implementation of the method mentioned in the previous commit.

--- src/token.c
+++ src/token.c
@@ -6,63 +6,64 @@
 void init_token_int_type(token_int_type * token)
 {
 	token->base.type = T_INT_TYPE;
-	token->base.token_pointer = token;
 }
 
 void init_token_int_value(token_int_value * token, int value)
 {
 	token->base.type = T_INT_VALUE;
-	token->base.token_pointer = token;
 	token->value = value;
 }
 
 void init_token_function(token_function * token, char * value)
 {
 	token->base.type = T_FUNCTION;
-	token->base.token_pointer = token;
 	token->name = value;
 }
 
 void init_token_opar(token_opar * token)
 {
 	token->base.type = T_OPAR;
-	token->base.token_pointer = token;
 }
 
 void init_token_cpar(token_cpar * token)
 {
 	token->base.type = T_CPAR;
-	token->base.token_pointer = token;
 }
 
 void init_token_obra(token_obra * token)
 {
 	token->base.type = T_OBRA;
-	token->base.token_pointer = token;
 }
 
 void init_token_cbra(token_cbra * token)
 {
 	token->base.type = T_CBRA;
-	token->base.token_pointer = token;
 }
 
 void init_token_return(token_return * token)
 {
 	token->base.type = T_RETURN;
-	token->base.token_pointer = token;
 }
 
 void init_token_semicolon(token_semicolon * token)
 {
 	token->base.type = T_SEMICOLON;
-	token->base.token_pointer = token;
 }
 
 void init_token_eof(token_eof * token)
 {
 	token->base.type = T_END_OF_FILE;
-	token->base.token_pointer = token;
+}
+
+void init_token_if(token_if * token)
+{
+	token->base.type = T_IF;
+}
+
+void init_token_boolean_op(token_boolean_op * token, char * op)
+{
+	token->base.type = T_BOOLEAN_OP;
+	token->op = op;
 }
 
 /*
@@ -80,6 +81,7 @@ void free_token_int_value(token_int_value * token)
 
 void free_token_function(token_function * token)
 {
+	free(token->name);
 	free(token);
 }
 
@@ -115,5 +117,16 @@ void free_token_semicolon(token_semicolon * token)
 
 void free_token_eof(token_eof * token)
 {
+	free(token);
+}
+
+void free_token_if(token_if * token)
+{
+	free(token);
+}
+
+void free_token_boolean_op(token_boolean_op * token)
+{
+	free(token->op);
 	free(token);
 }
